64de298ff140b670e210b84b6df2d93061ccc5d3
MoZadro <zadro.marko@nsoft.com>
2021-06-23 14:06:14 +0200

    Added stuff

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -3,6 +3,7 @@ package com.dockerforjavadevelopers.hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.UUID;
 
 @RestController
 public class HelloController {
@@ -10,6 +11,14 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         return "Hello World\n";
+
     }
-    
+    @RequestMapping("/healthz")
+    public boolean healthz() {
+        return true;
+    }
+    @RequestMapping("/logs")
+    public void logs(){
+        System.out.println(UUID.randomUUID.toString()+" godzadro3")
+    }
 }
